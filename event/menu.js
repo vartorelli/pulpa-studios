1605f1da1c49ac00b56099fae696b1bbeee0b5c7
vartorelli <facuvartorelli@gmail.com>
2025-02-17 15:47:21 -0300

    Arrreglando el menu

--- event/menu.js
+++ event/menu.js
@@ -1,17 +1,31 @@
 const d = document;
 
-const btnNavOpen = d.querySelector('.nav-btn-open')
-const btnNavClose = d.querySelector('.nav-btn-close')
-const nav = d.querySelector('.navegador-close')
+const btnNavOpen = d.querySelector(".nav-btn-open"),
+  btnNavClose = d.querySelector(".nav-btn-close"),
+  nav = d.querySelector(".navegador-close"),
+  btnsNav = d.querySelectorAll(".btn-nav");
 
 export const menu = () => {
-    d.addEventListener("click",e=>{
-        if(e.target === btnNavOpen){
-            nav.className = "navegador"
-            btnNavOpen.toggleAttribute('hidden')
-        }else if(e.target === btnNavClose){
-            nav.className = "navegador-close"
-            btnNavOpen.toggleAttribute('hidden')
+  d.addEventListener("click", (e) => {
+    if (nav.className === "navegador show") {
+        console.log(e.target)
+      if (e.target !== nav) {
+        nav.className = "navegador-close";
+        nav.classList.toggle("show");
+      }
+      btnsNav.forEach((btn) => {
+        if (e.target === btn) {
+          nav.className = "navegador-close";
+          nav.classList.toggle("show");
         }
-    })
-}
+      });
+    }
+    if (e.target === btnNavOpen) {
+      nav.className = "navegador";
+      nav.classList.toggle("show");
+    } else if (e.target === btnNavClose) {
+      nav.className = "navegador-close";
+      nav.classList.toggle("show");
+    }
+  });
+};
